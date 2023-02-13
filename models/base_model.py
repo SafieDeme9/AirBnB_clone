e633869fc4ad8d1f496347aca569a7e4789c628f
PrinceKan <princekan9.mk@gmail.com>
2023-02-13 01:18:39 +0000

    Store first object

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@
 """
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -23,6 +24,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """ Returns [<class name>] (<self.id>) <self.__dict__> """
@@ -34,6 +36,7 @@ class BaseModel:
             with the current datetime
         """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """ Returns a dictionary containing all keys/values of __dict__
