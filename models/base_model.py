7fa9c16b828a0297af264bf6ea8ed869edaea264
PrinceKan <princekan9.mk@gmail.com>
2023-02-13 00:56:49 +0000

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -4,16 +4,25 @@
     attributes/methods for other classes
 """
 import uuid
-import datetime
+from datetime import datetime
 
 
 class BaseModel:
     """ Defining the class """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ instantiation """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        if (len(kwargs) != 0):
+            kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            for k, v in kwargs.items():
+                if "__class__" not in k:
+                    setattr(self, k, v)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """ Returns [<class name>] (<self.id>) <self.__dict__> """
@@ -24,7 +33,7 @@ class BaseModel:
         """ pdates the public instance attribute updated_at
             with the current datetime
         """
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """ Returns a dictionary containing all keys/values of __dict__
